71228c9f4a4222f98b1555191b7c26d0dd8d723a
TAE <aekknon.phomsiriudom@mail.kmutt.ac.th>
2021-10-26 19:14:38 +0700

    [Edit] Chart user management 🤷‍♂️🤷‍♂️🤷‍♀️🤷‍♀️

--- src/components/dashboardCard/Chart.jsx
+++ src/components/dashboardCard/Chart.jsx
@@ -10,18 +10,20 @@ const Chart = (props) => {
 
   return (
     <div className={classes.chart}>
-      <h3 className={classes.chartTitle}>{props.appropiatePHQSeverity}</h3>
+      <h3 className={classes.chartTitle}>{props.appropiatePHQSeverity}{" "}{props.appropiatePHQSeverityScore}</h3>
       <GaugeChart
         nrOfLevels={5}
         colors={["#20CBFE", "#32D475", "#FFBC17", "#E76849", "#F14949"]}
         arcWidth={0.1}
-        percent={props.appropiatePHQSeverityScore/100}
+        percent={props.appropiatePHQSeverityScore/27}
+        hideText
         cornerRadius={4}
         arcPadding={0}
         style={chartStyle}
         className={classes.gaugeChart}
         textColor= "#00000"
       />
+        
     </div>
   );
 };
