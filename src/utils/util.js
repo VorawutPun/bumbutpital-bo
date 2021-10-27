17162980a2546053a0ebab93f33aad7fe0bf828b
Vorawut <vorawut.pun@mail.kmutt.ac.th>
2021-10-27 16:58:08 +0700

    [ADD] icon in sidebar

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,3 +1,11 @@
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import LoyaltyIcon from '@material-ui/icons/Loyalty';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import ForumIcon from '@material-ui/icons/Forum';
+
 export const depressionSeverity = [
   {
     severity: "Minimal Depression",
@@ -28,30 +36,37 @@ export const categoryItems = [
 export const menuItems = [
   {
     text: "Dashboard",
+    icon: <DashboardIcon color="primary" />,
     path: "/home",
   },
   {
     text: "Manage Contents",
+    icon: <LibraryBooksIcon color="primary" />,
     path: "/contents",
   },
   {
     text: "Manage Videos",
+    icon: <VideoLibraryIcon color="primary" />,
     path: "/videos",
   },
   {
     text: "Manage Users",
+    icon: <PeopleAltIcon color="primary" />,
     path: "/users",
   },
   {
     text: "Manage Promotions",
+    icon: <LoyaltyIcon color="primary" />,
     path: "/promotions",
   },
   {
     text: "Manage Hospitals",
+    icon: <LocalHospitalIcon color="primary" />,
     path: "/hospitals",
   },
   {
     text: "Forum",
+    icon: <ForumIcon color="primary" />,
     path: "/forum",
   },
 ];
