00e0f58dc2860e78a22dabe98e2d67dd1a126212
Vorawut <vorawut.pun@mail.kmutt.ac.th>
2021-11-16 15:36:28 +0700

    [ADD] layout 😀😀

--- src/App.js
+++ src/App.js
@@ -7,28 +7,8 @@ import {
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import "./App.css";
-// import Navbar from "./components/navbar/Navbar";
-import Sidebar from "./components/sidebar/Sidebar";
 import Authen from "./app/auth/Authen";
-import AddContent from "./app/content/AddContent";
-import AddPromotion from "./app/promotion/AddPromotion";
-import AddUser from "./app/user/AddUser";
-import AddVideo from "./app/video/AddVideo";
-import AddHospital from "./app/hospital/AddHospital";
-import Forum from "./app/forum/Forum";
-import Home from "./app/home/Home";
-import ListOfUsers from "./app/user/ListOfUser";
-import ManageVideo from "./app/video/Video";
-import ManageContent from "./app/content/Content";
-import ManageHospital from "./app/hospital/Hospital";
-import ManagePromotion from "./app/promotion/Promotion";
-// import EditUser from "./app/user/EditUser";
-import EditContent from "./app/content/EditContent";
-import EditPromotion from "./app/promotion/EditPromotion";
-import EditHospital from "./app/hospital/EditHospital";
-import EditVideo from "./app/video/EditVideo";
-import Profile from "./app/user/Profile";
-// import ManageUser from "./app/management/User";
+import Layout from "./app/Layout";
 
 function App() {
   const httpLink = createHttpLink({
@@ -58,33 +38,8 @@ function App() {
             {/* <Navbar /> */}
             <div className="container">
               <Switch>
-                <Route exact path="/" component={Authen} />
-                <Router>
-                  <Sidebar/>
-                    <Route exact path="/home" component={Home} />
-                    <Route path="/forum" component={Forum} />
-                    <Route path="/contents" component={ManageContent} />
-                    <Route path="/content/:contentID" component={EditContent} />
-                    <Route path="/createContent" component={AddContent} />
-                    <Route path="/videos" component={ManageVideo} />
-                    <Route path="/createVideo" component={AddVideo} />
-                    <Route path="/video/:videoID" component={EditVideo} />
-                    <Route path="/users" component={ListOfUsers} />
-                    <Route path="/user/:id" component={Profile} />
-                    <Route path="/createUser" component={AddUser} />
-                    <Route path="/promotions" component={ManagePromotion} />
-                    <Route path="/promotion/add" component={AddPromotion} />
-                    <Route
-                      path="/promotion/:promotionId"
-                      component={EditPromotion}
-                    />
-                    <Route path="/hospitals" component={ManageHospital} />
-                    <Route path="/hospital/add" component={AddHospital} />
-                    <Route
-                      path="/hospital/:hospitalID"
-                      component={EditHospital}
-                    />
-                </Router>
+                <Route exact path="/login" component={Authen} />
+                <Route path="/" component={Layout} />
               </Switch>
             </div>
           </div>
---
+++ src/app/Layout.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Sidebar from "../components/sidebar/Sidebar";
+import AddContent from "./content/AddContent";
+import AddPromotion from "./promotion/AddPromotion";
+import AddUser from "./user/AddUser";
+import AddVideo from "./video/AddVideo";
+import AddHospital from "./hospital/AddHospital";
+import Forum from "./forum/Forum";
+import Home from "./home/Home";
+import ListOfUsers from "./user/ListOfUser";
+import ManageVideo from "./video/Video";
+import ManageContent from "./content/Content";
+import ManageHospital from "./hospital/Hospital";
+import ManagePromotion from "./promotion/Promotion";
+// import EditUser from "./user/EditUser";
+import EditContent from "./content/EditContent";
+import EditPromotion from "./promotion/EditPromotion";
+import EditHospital from "./hospital/EditHospital";
+import EditVideo from "./video/EditVideo";
+import Profile from "./user/Profile";
+
+const Layout = () => {
+  return (
+    <>
+      <Sidebar />
+      <Switch>
+        <Route path="/home" component={Home} />
+        <Route path="/forum" component={Forum} />
+        <Route path="/contents" component={ManageContent} />
+        <Route path="/content/:contentID" component={EditContent} />
+        <Route path="/createContent" component={AddContent} />
+        <Route path="/videos" component={ManageVideo} />
+        <Route path="/createVideo" component={AddVideo} />
+        <Route path="/video/:videoID" component={EditVideo} />
+        <Route path="/users" component={ListOfUsers} />
+        <Route path="/user/:id" component={Profile} />
+        <Route path="/createUser" component={AddUser} />
+        <Route path="/promotions" component={ManagePromotion} />
+        <Route path="/promotion/add" component={AddPromotion} />
+        <Route path="/promotion/:promotionId" component={EditPromotion} />
+        <Route path="/hospitals" component={ManageHospital} />
+        <Route path="/hospital/add" component={AddHospital} />
+        <Route path="/hospital/:hospitalID" component={EditHospital} />
+      </Switch>
+    </>
+  );
+};
+
+export default Layout;
