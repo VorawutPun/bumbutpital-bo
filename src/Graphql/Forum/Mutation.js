e9fd9c55e77cdec9ad4e71a126449ed211b641ae
Vorawut <vorawut.pun@mail.kmutt.ac.th>
2021-11-19 00:59:46 +0700

    [FIX] mutation forum

--- src/Graphql/Forum/Mutation.js
+++ src/Graphql/Forum/Mutation.js
@@ -3,8 +3,7 @@ import { gql } from "@apollo/client";
 export const ANSWER_FORUM = gql`
   mutation answerForum($forumID: ID!, $answer: String!) {
     answerForum(forumID: $forumID, answer: $answer) {
-      forumID
-      answer
+      message
     }
   }
 `;
