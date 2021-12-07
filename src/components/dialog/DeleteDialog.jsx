caf06390fc9808f9ff7364554ceabc5281936091
Vorawut <vorawut.pun@mail.kmutt.ac.th>
2021-12-07 14:48:13 +0700

    [EDIT] can show deleteDialog

--- src/components/dialog/DeleteDialog.jsx
+++ src/components/dialog/DeleteDialog.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
@@ -7,28 +7,23 @@ import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
 const DeleteDialog = (props) => {
-    return (
-        <Dialog open={props.open} onClose={props.onClose}>
-        <DialogTitle>
-          {"Are you sure you want to delete?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            You are about to delete the content "{props.title}"
-            and all its data. Do you want to continue?
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={props.onClose}>Cancel</Button>
-          <Button
-            color="error"
-            onClick={props.onClick}
-          >
-            Delete
-          </Button>
-        </DialogActions>
-      </Dialog>
-    )
-}
+  return (
+    <Dialog open={props.open} onClose={props.handleDeleteFalse}>
+      <DialogTitle>{"Are you sure you want to delete?"}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          You are about to delete the content "{props.title}" and all its data.
+          Do you want to continue?
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={props.handleDeleteFalse}>Cancel</Button>
+        <Button color="secondary" onClick={props.handleDeleteTrue}>
+          Delete
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
 
-export default DeleteDialog
+export default DeleteDialog;
