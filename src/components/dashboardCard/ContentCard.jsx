552dfe79642ef610e96f9e8c9565e87c7ea073ac
Vorawut <vorawut.pun@mail.kmutt.ac.th>
2021-12-07 23:17:39 +0700

    [EDIT] content card style

--- src/components/dashboardCard/ContentCard.jsx
+++ src/components/dashboardCard/ContentCard.jsx
@@ -12,13 +12,72 @@ import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import { red } from "@material-ui/core/colors";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Grid } from "@material-ui/core";
+
+const ContentCard = (props) => {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+      <Card className={classes.root}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="recipe" className={classes.avatar}>
+              {props.title.charAt(0)}
+            </Avatar>
+          }
+          title={props.title}
+          subheader={props.createAt}
+        />
+        <CardMedia
+          className={classes.media}
+          image={props.pictureURL}
+          title={props.title}
+        />
+        <CardContent>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="p"
+            noWrap
+          >
+            {props.description}
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>{props.description}</Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+  );
+};
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
-    maxWidth: 500,
+    maxWidth: 600,
     backgroundColor: "#ecf2ff",
     borderRadius: "10px",
+    marginRight: "20px",
+    marginLeft: "20px",
+    justifyContent: "space-between"
   },
   media: {
     height: 0,
@@ -38,55 +97,5 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: red[500],
   },
 }));
-
-const ContentCard = (props) => {
-  const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
-  return (
-    <Card className={classes.root}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="recipe" className={classes.avatar}>
-            {props.title.charAt(0)}
-          </Avatar>
-        }
-        title={props.title}
-        subheader={props.createAt}
-      />
-      <CardMedia
-        className={classes.media}
-        image={props.pictureURL}
-        title={props.title}
-      />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p" noWrap>
-          {props.description}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>{props.description}</Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
-  );
-};
 
 export default ContentCard;
